7e666dcf40595bb986b43a5cc3c3f895c6cdc8b5
Jose Braulio Balanza Martinez <braulio-balanza@gmail.com>
2021-10-13 18:46:14 -0500

    Add strapi development backend

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,25 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+getDevelopmentGatsbyOptions = (env) => {
+      return {
+        apiURL: `http://localhost:1337`,
+      }
+}
+getProductionGatsbyOptions = (env) => {
+      return {
+        apiURL: env.STRAPI_URL,
+      }
+}
 
 module.exports = {
-  /* Your site config here */
   plugins: [
     'gatsby-plugin-postcss',
     {
     resolve: `gatsby-source-strapi`,
     options: {
-      apiURL: `https://strapi-production-5tz4r7de4a-uc.a.run.app`,
+      ...(process.env.NODE_ENV === 'development' 
+      ? getDevelopmentGatsbyOptions(process.env) 
+      : getProductionGatsbyOptions(process.env) ),
       queryLimit: 1000, // Default to 100
-      contentTypes: [`article`, `user`],
-      singleTypes: [`siteConfig`],
-      // Possibility to login with a strapi user, when content types are not publically available (optional).
-      loginData: {
-        identifier: "",
-        password: "",
-      },
+      collectionTypes: [`relics`],
     },
   },
   ],
