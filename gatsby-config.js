165beef8a34fa0d095797cfbb1c9ec03d268b807
Jose Braulio Balanza Martinez <braulio-balanza@gmail.com>
2021-10-13 15:28:09 -0500

    Add Strapi Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,21 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: ['gatsby-plugin-postcss'],
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+    resolve: `gatsby-source-strapi`,
+    options: {
+      apiURL: `https://strapi-production-5tz4r7de4a-uc.a.run.app`,
+      queryLimit: 1000, // Default to 100
+      contentTypes: [`article`, `user`],
+      singleTypes: [`siteConfig`],
+      // Possibility to login with a strapi user, when content types are not publically available (optional).
+      loginData: {
+        identifier: "",
+        password: "",
+      },
+    },
+  },
+  ],
 }
