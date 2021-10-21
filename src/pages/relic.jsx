b9e3eed8fd08a5403807c84ff131749d5e18e9f9
Jose Braulio Balanza Martinez <braulio-balanza@gmail.com>
2021-10-21 14:47:08 -0500

    Added relic page

---
+++ src/pages/relic.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Relic = () => {
+    return (
+        <div className="bg-black">
+            <h1 className="text-blue-50">This is a relic</h1>
+        </div>
+    )
+}
+
+export default Relic;
