5b46eac5ed9f5ce53320c9cf4d58422ab72caa54
Jose Braulio Balanza Martinez <braulio-balanza@gmail.com>
2021-10-21 12:10:41 -0500

    Gatsby looks for collection types seperately

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,31 @@
-getDevelopmentGatsbyOptions = (env) => {
-      return {
-        apiURL: `http://localhost:1337`,
-      }
+getDevelopmentGatsbyOptions = env => {
+  return {
+    apiURL: `http://localhost:1337`,
+    collectionTypes: [`relics`, `categories`],
+  }
 }
-getProductionGatsbyOptions = (env) => {
-      return {
-        apiURL: env.STRAPI_URL,
-        loginData: {
-        identifier: env.STRAPI_USERNAME,
-        password: env.STRAPI_PASSWORD,
-      },
-      }
+getProductionGatsbyOptions = env => {
+  return {
+    apiURL: env.STRAPI_URL,
+    loginData: {
+      identifier: env.STRAPI_USERNAME,
+      password: env.STRAPI_PASSWORD,
+      collectionTypes: [`relics`, `categories`],
+    },
+  }
 }
 
 module.exports = {
   plugins: [
-    'gatsby-plugin-postcss',
+    "gatsby-plugin-postcss",
     {
-    resolve: `gatsby-source-strapi`,
-    options: {
-      ...(process.env.NODE_ENV === 'development' 
-      ? getDevelopmentGatsbyOptions(process.env) 
-      : getProductionGatsbyOptions(process.env) ),
-      queryLimit: 1000, // Default to 100
-      collectionTypes: [`relics`, `categories`],
+      resolve: `gatsby-source-strapi`,
+      options: {
+        ...(process.env.NODE_ENV === "development"
+          ? getDevelopmentGatsbyOptions(process.env)
+          : getProductionGatsbyOptions(process.env)),
+        queryLimit: 1000, // Default to 100
+      },
     },
-  },
   ],
 }
