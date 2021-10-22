5e41ed0fd791e56f4bee1388c98d4379ecccdaa6
Jose Braulio Balanza Martinez <braulio-balanza@gmail.com>
2021-10-22 16:32:50 -0500

    Relic parses query from url

--- src/pages/relic.tsx
+++ src/pages/relic.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import { graphql } from 'gatsby'
+import { parse as parseQuery, ParsedQuery } from 'query-string'
+import { Location, RouteComponentProps, Router } from '@reach/router'
 
-const Relic = () => {
+const getQueriedRelic = (query: string): string | string[] | null => {
+    const queriedRelic = parseQuery(query);
+    const {relic} = queriedRelic;
+    return relic;
+}
+
+const Relic = ({location}: RouteComponentProps) => {
+    const searchQuery = location?.search ?? '';
     return (
-        <Nav />
+        <>
+            <Nav />
+                <h1>Your parsed query is {getQueriedRelic(searchQuery)}</h1>
+        </>
+
     )
 }
 
