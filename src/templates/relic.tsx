92dee31acc2dd6356511749d69a0b6fab164d15f
Jose Braulio Balanza Martinez <braulio-balanza@gmail.com>
2021-10-22 20:14:43 -0500

    Added react-markdown to relic page

--- src/templates/relic.tsx
+++ src/templates/relic.tsx
@@ -2,6 +2,7 @@ import { graphql } from 'gatsby'
 import React from 'react'
 import Nav from '../components/Nav'
 import Layout from '../components/Layout'
+import ReactMarkdown from 'react-markdown'
 
 const Relic = ({ data }: any) => {
     const relic = data.strapiRelics;
@@ -10,7 +11,7 @@ const Relic = ({ data }: any) => {
             <Nav />
             <Layout>
                 <h1>{relic.name}</h1>
-                <p>{relic.description}</p>
+                <ReactMarkdown children={relic.description} />
             </Layout>
         </>
 
@@ -22,6 +23,13 @@ query($id: String!) {
     strapiRelics(id: {eq: $id}) {
         name
         description
+        images {
+            localFile {
+                childrenImageSharp {
+                    gatsbyImageData(layout: FULL_WIDTH, placeholder: DOMINANT_COLOR)
+                }
+            }
+        }
     }
 }
 `;
