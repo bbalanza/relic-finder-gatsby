4aebd81648dbd192cfc25e1ac46c7045160a0444
Jose Braulio Balanza Martinez <braulio-balanza@gmail.com>
2021-10-23 20:18:35 -0500

    Changed logo from img to object

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 export default function Nav() {
     return (
-        <header className="mb-2" >
+        <header className="mb-2 sm:mb-4" >
             <div className="bg-gradient-to-r from-accent-100 via-accent-200 to-accent-300 h-2"></div>
-            <nav className="text-white bg-black h-32 flex items-center justify-center gap-2 flex-wrap ">
-                <img src='/logo.svg' className='h-20'></img>
+            <nav className="text-green-900 bg-black h-32 flex items-center justify-center gap-2 flex-wrap ">
+                <object className="h-5/6 " data='/logo.svg' type='image/svg+xml'></object>
             </nav>
         </header>
     )
