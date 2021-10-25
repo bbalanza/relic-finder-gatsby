bd2571d4eb61aced0792b715bd0f8d180f1a4ffa
Jose Braulio Balanza Martinez <braulio-balanza@gmail.com>
2021-10-25 18:11:28 -0500

    Erased some imports

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,9 @@
-import React, { ReactChild, ReactFragment, ReactNode } from "react";
+import React from "react";
 
 const Layout: React.FC = (props) => {
     return (
         <div
-        className={`container space-y-2`}
+        className={`container space-y-5 block`}
         >
         {props.children}
         </div>
