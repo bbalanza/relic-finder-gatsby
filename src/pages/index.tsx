83537739cb24a2fbecaa115e419f51e40c3f2699
Jose Braulio Balanza Martinez <braulio-balanza@gmail.com>
2021-10-28 19:04:27 -0500

    Add QR scanner v1

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Nav from "../components/Nav"
+import { Scanner} from "../components/Scanner"
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <Scanner />
+    </>
+  )
+}
