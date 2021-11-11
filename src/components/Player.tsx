83d2d646d59d5e786903f1b3e524b9b6384e3900
Jose Braulio Balanza Martinez <braulio-balanza@gmail.com>
2021-11-11 13:11:13 -0500

    Added audio player

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,11 +1,20 @@
 import React from "react"
+import { ReactElement } from "react-markdown/lib/react-markdown";
 
-const Player: React.FC<Partial<Element>> = (): React.ReactElement => {
+type Player = {
+    audioUrl: string
+} & Partial<Element>
 
-    return (
-    <>
-         
+const Player: React.FC<Player> = (props: Player): ReactElement => {
+    return <>
+        <audio
+            controls
+            src={props.audioUrl}
+        >
+            Your browser does not support the
+            <code>audio</code> element.
+        </audio>
     </>
-    );
-
 }
+
+export default Player;
--- src/templates/relic.tsx
+++ src/templates/relic.tsx
@@ -6,16 +6,19 @@ import Nav from '../components/Nav'
 import Layout from '../components/Layout'
 import Preview from '../components/Preview'
 import Button from '../components/Button'
+import Player from '../components/Player'
 
 const Relic = ({ data }: any) => {
     const relic = data.strapiRelics;
     const previewImage = relic.images.localFile;
+    const audio = relic.audio_description.localFile.url;
     return (
         <>
             <Nav />
             <Layout>
                 <Preview localFile={previewImage} name={relic.name} />
                 <ReactMarkdown children={relic.description} />
+                <Player audioUrl={audio} />
                 <div className="flex justify-center">
                     <h4 className="mt-5">
                         <Button to="/" >Return to Camera</Button>
@@ -47,3 +50,5 @@ query($id: String!) {
 }
 `;
 export default Relic;
+
+
