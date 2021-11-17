053304dfea551dc10086e3ea55d35bd5912b06ba
Jose Balanza <braulio.balanza@gmail.com>
2021-11-17 23:19:06 +0000

    Added Nav to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-
+import Nav from "./Nav"
 const Layout: React.FC = (props) => {
     return (
         <div
-        className={`layout`}
+            className={`layout`}
         >
-        {props.children}
+            <Nav className='h-1/4 w-full' />
+            {props.children}
         </div>
     );
-} 
+}
 export default Layout;
