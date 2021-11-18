c45ac6961e02684751a669a1e29911439f8abacd
Jose Balanza <braulio.balanza@gmail.com>
2021-11-18 16:46:36 +0000

    Centered audio player

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -7,13 +7,17 @@ type Player = {
 
 const Player: React.FC<Player> = (props: Player): ReactElement => {
     return <>
-        <audio
-            controls
-            src={props.audioUrl}
+        <div
+            className="flex-auto self-center"
         >
-            Your browser does not support the
-            <code>audio</code> element.
-        </audio>
+            <audio
+                controls
+                src={props.audioUrl}
+            >
+                Your browser does not support the
+                <code>audio</code> element.
+            </audio>
+        </div>
     </>
 }
 
