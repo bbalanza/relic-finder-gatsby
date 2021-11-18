6945c0f0517498eacd5bdee376f64ffda23d792a
Jose Balanza <braulio.balanza@gmail.com>
2021-11-18 20:39:40 +0000

    Refactored Scanner a bit

--- src/components/Scanner/Scanner.tsx
+++ src/components/Scanner/Scanner.tsx
@@ -1,23 +1,10 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+import { navigate } from "gatsby";
 import { Html5Qrcode } from "html5-qrcode";
 import { QrcodeErrorCallback, QrcodeSuccessCallback } from "html5-qrcode/esm/core";
-import { navigate } from "gatsby";
 import { Html5QrcodeCameraScanConfig, Html5QrcodeFullConfig } from "html5-qrcode/esm/html5-qrcode";
 
-/* TODO: Test!!! */
-const stripUrlParams = (decodedText: string): string => {
-    const parameterRegEx = new RegExp(/\/{1}[0-9 A-F]{4}/, 'g')
-    return parameterRegEx.exec(decodedText)?.toString() ?? '';
-}
-
-const getRelicID = (url:string): string => stripUrlParams(url)
-
-const getCameraRatio = (screenWidth: number, screenHeight: number): number => {
-   return (16/9) * .76; 
-}
-
-const Scanner: React.FC = () => {
+const Scanner = (props: ScannerProps) => {
     const scannerDebugConfig: Html5QrcodeFullConfig = {
         verbose: false,
     }
@@ -31,11 +18,17 @@ const Scanner: React.FC = () => {
         const qrCodeErrorCallback: QrcodeErrorCallback = (errorMessage, error) => {
             console.log(error)
         }
-        const qrScannerConfig: Html5QrcodeCameraScanConfig = { fps: 10, aspectRatio: getCameraRatio(window.screen.width, window.screen.height), qrbox: 250 };
-        html5QrCode.start({ facingMode: "environment" }, qrScannerConfig, qrCodeSuccessCallback, qrCodeErrorCallback);
+        const qrScannerConfig: Html5QrcodeCameraScanConfig = { 
+            fps: 10, 
+            aspectRatio: getCameraRatio(window.screen.width, window.screen.height), 
+            qrbox: 250 };
+        const cameraConfig = { 
+            facingMode: "environment"
+        };
+        html5QrCode.start(cameraConfig, qrScannerConfig, qrCodeSuccessCallback, qrCodeErrorCallback);
     }, []);
 
-    return (<div id={'reader'} />);
+    return <div id={'reader'} className={`${props.className}`}/>;
 }
 
 export default Scanner;
---
+++ src/components/Scanner/helpers.tsx
@@ -0,0 +1,10 @@
+/* TODO: Test!!! */
+const stripUrlParams = (decodedText: string): string => {
+    const parameterRegEx = new RegExp(/\/{1}[0-9 A-F]{4}/, 'g')
+    return parameterRegEx.exec(decodedText)?.toString() ?? '';
+}
+const getRelicID = (url:string): string => stripUrlParams(url)
+
+const getCameraRatio = (screenWidth: number, screenHeight: number): number => {
+   return (16/9) * .76; 
+}
