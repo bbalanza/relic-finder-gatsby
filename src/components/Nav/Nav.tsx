4577938de624bf3d36ea808c8714e998c6bdd821
Jose Balanza <braulio.balanza@gmail.com>
2022-01-12 19:43:56 +0000

    Added tests for Nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,7 +5,7 @@ const Nav = (props: NavProps) => {
         <header {...props} className={`${props.className ?? ''} h-1/4 w-full flex flex-col justify-start`}>
             <div className={`h-4 flex-initial w-full nav-bar-gradient`}></div>
             <nav className={`flex-grow h-5/6 w-full bg-black flex items-center justify-center`}>
-                <img className="h-1/2" src={Logo}/>
+                <img className="h-1/2" title='logo' src={Logo}/>
             </nav>
         </header>
     )
