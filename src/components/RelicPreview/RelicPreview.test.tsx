2f7f4a1a991671e402b4042424eb24c647846c7d
Jose Balanza <braulio.balanza@gmail.com>
2022-01-12 03:41:08 +0000

    Added RelicPreview test

---
+++ src/components/RelicPreview/RelicPreview.test.tsx
@@ -0,0 +1,36 @@
+import { screen, render } from '@testing-library/react'
+import { RelicPreview } from '.'
+import React from 'react'
+import {getImage, GatsbyImage, ImageDataLike} from "gatsby-plugin-image"
+
+// Credit to https://bradgarropy.com/blog/mocking-gatsby-image for this amazing mock
+jest.mock('gatsby-plugin-image', () => {
+    const React = require('react');
+    const originalModule = jest.requireActual('gatsby-plugin-image');
+    const mockImage = ({imgClassName, ...props}: {imgClassName: string, props: any}) => 
+    React.createElement('img', {
+        ...props,
+        className: imgClassName,
+        image: 'test.png'
+    })
+    return {
+        __esModule: true,
+        ...originalModule,
+        GatsbyImage: jest.fn().mockImplementation(mockImage),
+        StaticImage: jest.fn().mockImplementation(mockImage),
+    };
+})
+
+describe('Test RelicPreview', () => {
+    it('Shows the picture passed as prop', () => {
+        render(<RelicPreview localFile='' />)
+        expect(screen.getByAltText('Relic Image').getAttribute('image')).toBe('test.png');
+    })
+    it('Displays children components', () => {
+        render(
+        <RelicPreview localFile=''>
+            <h1 data-testid='testTarget' >This was rendered</h1>
+        </RelicPreview>)
+        expect(screen.getByTestId('testTarget').textContent).toBe('This was rendered')
+    })
+})
