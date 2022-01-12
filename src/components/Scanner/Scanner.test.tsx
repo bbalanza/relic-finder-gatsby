3036caf6f4c752edda24c939cc5c974733f7b7ad
Jose Balanza <braulio.balanza@gmail.com>
2022-01-12 02:00:34 +0000

    Added Scanner test

---
+++ src/components/Scanner/Scanner.test.tsx
@@ -0,0 +1,11 @@
+import { screen, render} from '@testing-library/react';
+import { Scanner } from './index';
+import React from 'react'
+
+describe('Test Scanner component', () => {
+    it('Renders the QR Scanner component.', () => {
+        window.alert = (msg: string) => {console.log(msg)}
+        render(<Scanner data-testid="scanner"/>)
+        expect(screen.getByTestId('scanner').id).toBe('reader');
+    })
+})
