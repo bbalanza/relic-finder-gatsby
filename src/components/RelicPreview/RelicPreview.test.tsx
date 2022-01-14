3b18387720158bc2c31a0c82c80fda2fd39ed305
Jose Balanza <braulio.balanza@gmail.com>
2022-01-14 19:34:36 +0000

    Created gatsby-plugin-image mock

--- src/components/RelicPreview/RelicPreview.test.tsx
+++ src/components/RelicPreview/RelicPreview.test.tsx
@@ -2,24 +2,6 @@ import { screen, render } from '@testing-library/react'
 import { RelicPreview } from '.'
 import React from 'react'
 
-// Credit to https://bradgarropy.com/blog/mocking-gatsby-image for this amazing mock
-jest.mock('gatsby-plugin-image', () => {
-    const React = require('react');
-    const originalModule = jest.requireActual('gatsby-plugin-image');
-    const mockImage = ({imgClassName, ...props}: {imgClassName: string, props: any}) => 
-    React.createElement('img', {
-        ...props,
-        className: imgClassName,
-        image: 'test.png'
-    })
-    return {
-        __esModule: true,
-        ...originalModule,
-        GatsbyImage: jest.fn().mockImplementation(mockImage),
-        StaticImage: jest.fn().mockImplementation(mockImage),
-    };
-})
-
 describe('Test RelicPreview', () => {
     it('Shows the picture passed as prop', () => {
         render(<RelicPreview localFile='' />)
