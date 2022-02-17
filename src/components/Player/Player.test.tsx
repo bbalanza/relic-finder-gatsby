6599904b6f5eeeb0824c257ee2b2456cb87c69e6
Jose Balanza <braulio.balanza@gmail.com>
2022-02-17 22:32:44 +0000

    Fixed tests

--- src/components/Player/Player.test.tsx
+++ src/components/Player/Player.test.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import { screen, render} from '@testing-library/react'
 import { Player } from '.'
 
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 describe('Test audio player', () => {
-    it('It renders an audio player with the specified audio file as src', () => {
+    it('It renders an audio player with the specified audio file as src', async () => {
         render(<Player src='test.mp3' />)
-        expect(screen.getByTitle('audio-player').getAttribute('src')).toBe('test.mp3')
+        await sleep(100)
+        expect(screen.getByTitle('audio-player').children[0].getAttribute('src')).toBe('test.mp3')
     })
 })
--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -5,7 +5,7 @@ const Player = (props: Player) => {
     return <div
             className={`${props.className ?? ''} flex-grow self-center h-10 w-full`}
         >
-            <ReactPlayer url={props.src} controls height='inherit' width='inherit' config={{
+            <ReactPlayer url={props.src} controls height='inherit' width='inherit' title='audio-player' config={{
                 file: {
                     forceAudio: true
                 }
